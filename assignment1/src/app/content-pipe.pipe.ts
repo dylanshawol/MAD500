2946aa9edf5eeb46ed5f429d17d31beaa8975762
dylanshawol <dylan.shawol01@stclairconnect.ca>
2021-10-05 11:22:13 -0400

    Assignment 3 commit

---
+++ assignment1/src/app/content-pipe.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from "./helper-files/content-interface";
+
+@Pipe({
+  name: 'contentPipe'
+})
+export class ContentPipePipe implements PipeTransform {
+
+  transform(contentArray: Content[], type: string): Content[] {
+    return contentArray.filter(c => c.type?.includes(type))
+  }
+}
